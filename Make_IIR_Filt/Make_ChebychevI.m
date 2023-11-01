c30d52a4be47546939b439fbb6153def4bfab06a
Philis420 <149073630+Philis420@users.noreply.github.com>
2023-11-01 15:53:14 +0100

    Make IIR filter code tilføjet
    Hej Du :)

---
+++ Make_IIR_Filt/Make_ChebychevI.m
@@ -0,0 +1,7 @@
+function [Bn,An] = Make_ChebychevI(n,fs,CuttOffFreqencies,ApproxType,RippledB)
+            
+    TypeFilt = Make_Type(ApproxType);
+    NormCuttOffFreqencies = CuttOffFreqencies./(fs/2);
+    [Bn, An] = cheby1(n,RippledB,NormCuttOffFreqencies,TypeFilt);
+
+end
