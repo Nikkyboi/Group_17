fa5f21f9ce097bf22beca87a40f81644e340ae4c
Philis420 <149073630+Philis420@users.noreply.github.com>
2023-11-21 18:18:59 +0100

    Updatering af Plotting del

--- Big_Boss_Funktionen/Plotting.m
+++ Big_Boss_Funktionen/Plotting.m
@@ -1,7 +1,33 @@
 
 
+%Code for generating plots related to time signal.
+if XAxType=="lin"
+    TimeAxis;
+elseif XAxType=="log"
+    TimeAxis = log10(TimeAxis);
+end
 
+if YAxType == "lin"
+    Signal;
+    FilteredSignal;
+elseif YAxType == "log"
+    Signal = log10(Signal);
+    FilteredSignal = log10(FilteredSignal);
+end
 
+figure()
+hold on
+plot(TimeAxis,Signal,'r')
+plot(TimeAxis,FilteredSignal,'b')
+xlim([0 xLimTime])
+ylim([-ylimSig ylimSig]) 
+xlabel('Time in '+XAxType+'(s)')
+ylabel('Time in '+YAxType+'(Amplitude)')
+legend('Orginal signal','Filtered Signal')
+title('Orginal signal vs. filteret signal')
+ax = gca; 
+ax.FontSize = 13; 
+%Code for generation plots related to fourier spectrum.
 
 %Code for generateing plots related to filter.
 figure()
